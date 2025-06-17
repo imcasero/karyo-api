d797a1bb1d4ff506d2cc80950d1878b9683ee9e2
Imcasero <diegocaserosmr@gmail.com>
2025-06-17 17:14:55 +0200

    feat: enable CORS in the main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: [process.env.FRONTEND_ORIGIN],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
